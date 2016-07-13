e086d72135741d0f7f904425ec018a820f9f7e7a
Kilbourne <luca.castellone@gmail.com>
2016-07-14 01:41:25 +0200

    menu refactor

--- web/app/themes/menthalia/templates/header.php
+++ web/app/themes/menthalia/templates/header.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
   $site_claim=!!get_field('claim_home','options')?get_field('claim_home','options'):get_bloginfo( 'description' );
  ?>
 <header class="banner skrollable "  data-0="height:158px;"  data-80="height:118px;">
  <h1 class="site-claim"><?php echo $site_claim; ?></h1>
-
+<div class="menu-container"><?php echo do_shortcode('[responsive_menu_pro]') ?></div>
 </header>
-<?php echo do_shortcode('[responsive_menu_pro]') ?>
